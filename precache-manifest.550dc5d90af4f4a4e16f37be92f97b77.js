e7dc73af743020e983955d38936f8c93afaa6a42
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-10-24 19:34:56 +0200

    Updates

---
+++ precache-manifest.550dc5d90af4f4a4e16f37be92f97b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa04ca33c9a189ad366626c50185d6d",
+    "url": "/Centmsn/React-calculator/index.html"
+  },
+  {
+    "revision": "bbff00be36889271da04",
+    "url": "/Centmsn/React-calculator/static/css/main.3fda5da2.chunk.css"
+  },
+  {
+    "revision": "986f6b587cc7f28e3c1d",
+    "url": "/Centmsn/React-calculator/static/js/2.e2b8d282.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Centmsn/React-calculator/static/js/2.e2b8d282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbff00be36889271da04",
+    "url": "/Centmsn/React-calculator/static/js/main.b255cfb0.chunk.js"
+  },
+  {
+    "revision": "6bb944ce02840a8f3e51",
+    "url": "/Centmsn/React-calculator/static/js/runtime-main.01feee70.js"
+  }
+]);
